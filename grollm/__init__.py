4e56d82a9f1bc69d32cfdf5f73c435e2303eda57
Bijon Guha <bijon.guha@siemens.com>
2025-01-10 17:33:16 +0530

    bumped openai version

--- grollm/__init__.py
+++ grollm/__init__.py
@@ -1,10 +1,11 @@
 
-__version__ = "0.0.9a1"
+__version__ = "0.0.9a3"
 
 import os
 from .openai_gro import OpenAI_Grollm
 from .anthropic_gro import Anthropic_Grollm
 from .gemini_gro import Gemini_Grollm
+from .azureopenai_gro import AzureOpenAI_Grollm
 
 __all__ = (
     "__version__",
